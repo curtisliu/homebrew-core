3a1b887fe2c41cd37619a71df2c6f2387558cb6f
Curtis Liu <curtis@amplitude.com>
2020-09-14 15:26:47 -0700

    Add yarn@1.19.1

---
+++ Formula/yarn.rb
@@ -0,0 +1,26 @@
+class Yarn < Formula
+  desc "JavaScript package manager"
+  homepage "https://yarnpkg.com/"
+  # Should only be updated if the new version is listed as a stable release on the homepage
+  url "https://yarnpkg.com/downloads/1.19.1/yarn-v1.19.1.tar.gz"
+  sha256 "34293da6266f2aae9690d59c2d764056053ff7eebc56b80b8df05010c3da9343"
+
+  bottle :unneeded
+
+  depends_on "node"
+
+  conflicts_with "hadoop", :because => "both install `yarn` binaries"
+
+  def install
+    libexec.install Dir["*"]
+    (bin/"yarn").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX, :NPM_CONFIG_PYTHON => "/usr/bin/python"
+    (bin/"yarnpkg").write_env_script "#{libexec}/bin/yarn.js", :PREFIX => HOMEBREW_PREFIX, :NPM_CONFIG_PYTHON => "/usr/bin/python"
+    inreplace "#{libexec}/package.json", '"installationMethod": "tar"', '"installationMethod": "homebrew"'
+  end
+
+  test do
+    (testpath/"package.json").write('{"name": "test"}')
+    system bin/"yarn", "add", "jquery"
+    system bin/"yarn", "add", "fsevents", "--build-from-source=true"
+  end
+end
